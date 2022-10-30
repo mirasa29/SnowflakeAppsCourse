1957854f99ff3c7f8cdb3e16f7fa1e02f131b9d9
Arjel Miras <jelats@gmail.com>
2022-10-30 17:35:27 +1100

    Updated the breakfast menu with emojis. Added section for build smoothie

--- Scripts/streamlit_app.py
+++ Scripts/streamlit_app.py
@@ -2,8 +2,13 @@ import streamlit
 
 streamlit.title('My Parent New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boilded Free-Range Egg')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boilded Free-Range Egg')
+
+
+streamlit.title('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
 
