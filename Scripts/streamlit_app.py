4f0bc444079a41eaf5d0c3247058c4a1958b8677
Arjel Miras <jelats@gmail.com>
2022-10-30 17:29:46 +1100

    Added initial breakfast menu

--- Scripts/streamlit_app.py
+++ Scripts/streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parent New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boilded Free-Range Egg')
+
