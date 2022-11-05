e5a330846034c3a0aa0c2f6c7ebc2c100efec5f6
Arjel Miras <jelats@gmail.com>
2022-11-05 23:48:11 +1100

    fixed syntax error in SQL

--- Scripts/streamlit_app.py
+++ Scripts/streamlit_app.py
@@ -66,7 +66,7 @@ if streamlit.button('Get Fruit Load List'):
 # New section to accept input fruit to add
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-         my_cur.execute(insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+         my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
          return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
